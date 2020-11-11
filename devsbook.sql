0f36b02853caf994effda98ea822166305e76a81
Pedro Miguel Sousa <sousacmpedro@gmail.com>
2020-11-11 18:47:48 -0300

    Envio de Banco de dados atualizado

--- devsbook.sql
+++ devsbook.sql
@@ -1,87 +1,130 @@
--- phpMyAdmin SQL Dump
--- version 5.0.2
--- https://www.phpmyadmin.net/
+-- MySQL dump 10.17  Distrib 10.3.25-MariaDB, for debian-linux-gnu (x86_64)
 --
--- Host: localhost
--- Tempo de geração: 07/08/2020 às 15:24
--- Versão do servidor: 10.4.13-MariaDB
--- Versão do PHP: 7.4.9
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: devsbook
+-- ------------------------------------------------------
+-- Server version	10.3.25-MariaDB-0+deb10u1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Banco de dados: `devsbook`
+-- Table structure for table `postcomments`
 --
 
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `postcomments`
---
-
+DROP TABLE IF EXISTS `postcomments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `postcomments` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `id_post` int(11) NOT NULL,
   `id_user` int(11) NOT NULL,
   `created_at` datetime NOT NULL,
-  `body` text NOT NULL
+  `body` text NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Estrutura para tabela `postlikes`
+-- Dumping data for table `postcomments`
 --
 
+LOCK TABLES `postcomments` WRITE;
+/*!40000 ALTER TABLE `postcomments` DISABLE KEYS */;
+/*!40000 ALTER TABLE `postcomments` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `postlikes`
+--
+
+DROP TABLE IF EXISTS `postlikes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `postlikes` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `id_post` int(11) NOT NULL,
   `id_user` int(11) NOT NULL,
-  `created_at` datetime NOT NULL
+  `created_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Estrutura para tabela `posts`
+-- Dumping data for table `postlikes`
 --
 
+LOCK TABLES `postlikes` WRITE;
+/*!40000 ALTER TABLE `postlikes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `postlikes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `posts`
+--
+
+DROP TABLE IF EXISTS `posts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `posts` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `id_user` int(11) NOT NULL,
   `type` varchar(20) NOT NULL,
   `created_at` datetime NOT NULL,
-  `body` text NOT NULL
+  `body` text NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Estrutura para tabela `userrelations`
+-- Dumping data for table `posts`
 --
 
+LOCK TABLES `posts` WRITE;
+/*!40000 ALTER TABLE `posts` DISABLE KEYS */;
+/*!40000 ALTER TABLE `posts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `userrelations`
+--
+
+DROP TABLE IF EXISTS `userrelations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `userrelations` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_from` int(11) NOT NULL,
-  `user_to` int(11) NOT NULL
+  `user_to` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Estrutura para tabela `users`
+-- Dumping data for table `userrelations`
 --
 
+LOCK TABLES `userrelations` WRITE;
+/*!40000 ALTER TABLE `userrelations` DISABLE KEYS */;
+/*!40000 ALTER TABLE `userrelations` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `email` varchar(100) NOT NULL,
   `password` varchar(200) NOT NULL,
   `name` varchar(100) NOT NULL,
@@ -90,78 +133,28 @@ CREATE TABLE `users` (
   `work` varchar(100) DEFAULT NULL,
   `avatar` varchar(100) NOT NULL DEFAULT 'default.jpg',
   `cover` varchar(100) NOT NULL DEFAULT 'cover.jpg',
-  `token` varchar(200) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `token` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Índices de tabelas apagadas
+-- Dumping data for table `users`
 --
 
---
--- Índices de tabela `postcomments`
---
-ALTER TABLE `postcomments`
-  ADD PRIMARY KEY (`id`);
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (6,'sousacmpedro@gmail.com','$2y$10$RZiMmRImd9h21nCn39jAUuixoNpL7TxGFYfq1wGLxCN0PnQX9zgzq','Pedro Miguel da Cruz de Sousa','1986-04-15',NULL,NULL,'default.jpg','cover.jpg','cf43d53e26ecfd995fcec85f8946765a');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Índices de tabela `postlikes`
---
-ALTER TABLE `postlikes`
-  ADD PRIMARY KEY (`id`);
-
---
--- Índices de tabela `posts`
---
-ALTER TABLE `posts`
-  ADD PRIMARY KEY (`id`);
-
---
--- Índices de tabela `userrelations`
---
-ALTER TABLE `userrelations`
-  ADD PRIMARY KEY (`id`);
-
---
--- Índices de tabela `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT de tabelas apagadas
---
-
---
--- AUTO_INCREMENT de tabela `postcomments`
---
-ALTER TABLE `postcomments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de tabela `postlikes`
---
-ALTER TABLE `postlikes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de tabela `posts`
---
-ALTER TABLE `posts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de tabela `userrelations`
---
-ALTER TABLE `userrelations`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de tabela `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-COMMIT;
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-11-11 18:46:09
